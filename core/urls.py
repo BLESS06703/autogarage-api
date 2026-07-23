e90161bf278c6ab27b245f01966c75d1b0e4fadf
blesskaunda056 <blesskaunda056@gmail.com>
2026-07-23 14:13:22 +0200

    Add homepage and set DEBUG=False

--- core/urls.py
+++ core/urls.py
@@ -5,3 +5,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
+
+from django.http import HttpResponse
+def home(request):
+    return HttpResponse("<h1>AutoGarage API</h1><p>API is running. Visit <a href='/api/'>/api/</a></p>")
+urlpatterns.insert(0, path('', home))
