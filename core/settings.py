e91abaa512c2e703d085eac227e568732d68a9cf
blesskaunda056 <blesskaunda056@gmail.com>
2026-07-30 21:04:55 +0200

    Temporarily enable DEBUG to trace cart error

--- core/settings.py
+++ core/settings.py
@@ -3,7 +3,7 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-blessgarage-dev-key-2024')
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
